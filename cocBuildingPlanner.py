bc09ccbc44373ac0ec69f63e9d29e06bf0efe9d3
Maxis <maxis1718@gmail.com>
2014-11-22 01:03:22 +0800

    Add update-working feature in updater

--- cocBuildingPlanner.py
+++ cocBuildingPlanner.py
@@ -358,18 +358,36 @@ def update_level(current_levels, new_level):
         pass
     return current_levels
 
-def updater(now, working, worker, nextBuilding, bulidings, pausedTime):
+def updater(now, working, worker, nextBuilding, buildings, pausedTime):
     """
+    Function
+    ========
+    Update `now`, `working`, `worker` lists
+
     Parameters
     ==========
-    working=[('mortar',2,1),('camp',1,2),('mortar',2,3),('camp',1,4),('camp',1,5)]
-    worker=[1,2,3,4,5]
-    now={'mortar':[1,1,1,0],'cannon':[0,0,0,0]}
-    nextBuilding=('cannon', 1) 
-    pausedTime=1
-    ==========
+    working = [('mortar',2,1), ('camp',1,2), ('mortar',2,3), ('camp',1,4), ('camp',1,5)]
+    worker = [1,2,3,4,5]
+    now = {'mortar':[1,1,1,0],'cannon':[0,0,0,0]}
+    nextBuilding = ('cannon', 1) 
+    pausedTime = 1
+
+    Expected outputs
+    ================
+    > input
+    working : [('mortar',2,1), ('camp',1,2), ('mortar',2,3), ('camp',1,4), ('camp',1,5)]
+     
+    > subtract `pausedTime`
+    working: [('mortar',2,0), ('camp',1,1), ('mortar',2,2), ('camp',1,3), ('camp',1,4)]
+     
+    > emit ('mortar',2,0)
+    working: [('camp',1,1), ('mortar',2,2), ('camp',1,3), ('camp',1,4)]
+     
+    > add ('cannon', 1, ?)  ## ? is obtained from `buildings.json`
+    working: [('camp',1,1), ('mortar',2,2), ('camp',1,3), ('camp',1,4), ('cannon', 1, 1)]
+
     """
-    
+    ## update `now`
     for name, level, remaining in filter(lambda x:max(x[2]-pausedTime,0) == 0, working):
         # create value
         if name not in now:
@@ -378,94 +396,111 @@ def updater(now, working, worker, nextBuilding, bulidings, pausedTime):
         else:
             now[name] = update_level(current_levels=now[name], new_level=level) 
         # update `new_working` values
-    working = filter(lambda x:max(x[2]-pausedTime,0) > 0, working)
-    worker = lambda x:max(x[2]-pausedTime,0), working    
 
-    return 
+    ## update `working`
+    # 1. subtract `pausedTime`
+    working = [ (name, level, max(remaining - pausedTime ,0) ) for name, level, remaining in working ]
+
+    # 2. emit element(s) with zero remaining day, i.e., `completed`
+    completed = filter(lambda x:x[2] == 0, working)
+    working = filter(lambda x:x[2] > 0, working)
+
+    # 3.1 lookup next remaining
+    next_name, next_level = nextBuilding
+    next_remaining = buildings[next_name][next_level]['time']
+
+    # 3.2 add `nextBuilding`
+    working.append( (next_name, next_level, next_remaining) )
+
+
+
+    ## update worker
+    # worker = lambda x:max(x[2]-pausedTime,0), working    
+
+    # return 
 
 
 
 
-
-
-
-nextBuildings = getNextBuildings(now,working,obj,nextWorker)
-# print 'nextBuildings: ',nextBuildings
-maxCollision = getMaxCollision(nextBuildings,buildings,maxLoot)
-# print 'getMaxCollision: ',maxCollision
-time = 0
-tempTI = 0
-while not finish(now,working,obj):
-    timeInterval = getTimeInterval(nextWorker) + tempTI
-    tempTI = timeInterval
-    loot = addLoot(loot,earn,timeInterval)
-    time = addTime(time,timeInterval)
-    minInterval = getMinInterval(nextBuildings,buildings,earn)
-    maxInterval = getMaxInterval(nextBuildings,buildings,maxLoot,loot,earn)
-#    print 'maxInterval: ',maxInterval,',minInterval: ',minInterval
-    available = findAvailable(nextBuildings,buildings,loot,minInterval,maxInterval,maxCollision,worker,working)
-#    print 'available: ',available
-#    print '>'*20
-#    print 'timeInterval:',timeInterval
-#    print 'time:',time
-#    print 'tempTI:',tempTI
-
-#    print '<'*20
-
-    if available:
-        if len(available) == 1:
-            nextBuilding = available
-        else:
-            nextBuilding = findBest(available,working,buildings,earn,bound)
-    #    
-#        print 'nextBuilding:',nextBuilding
-#        worker = upWorker(nextBuilding,buildings,worker,timeInterval)
-        # print 'worker:',worker
-
-#        now = upNow(working,now)
-#        print 'now:',now
-
-#        print '>> working', working
-#        working = upWorking(working, nextBuilding,buildings,worker, timeInterval)
-#        print '<< working:',working
-
-        require=getRequire(nextBuilding,buildings,loot)
-        
-        bn, blv = nextBuilding
-        print 'Start build:',bn
-        print 'level:',blv
-        print 'require:',buildings[bn][blv]['cost'],' ',buildings[bn]['type']
-        try:
-            print 'require per day:',[require[0]/timeInterval,require[1]/timeInterval,require[2]/timeInterval]
-        except ZeroDivisionError:
-            print 'wtf'
-        tempTI = 0
-        pausedTime=timeInterval
-    else:
-        pausedTime=getPausedTime(pausedTime)
-#        worker=waitWorker(waitTime,worker)
-
-#        working=waitWorking(waitTime,working)
-
-#        now=examinigDone(working,now)
-#        print 'else'
-        # no available building waiting resourse 
-        # TBD
-    print '===before==='
-    print 'worker:',worker
-    print 'working:',working
-    print 'now:',now
-    print '============'
-    updater(now, working, worker, nextBuilding, buildings, pausedTime)
-    print '===after==='
-    print 'worker:',worker
-    print 'working:',working
-    print 'now:',now
-    print '============'
-    sortedWorker = sorted(worker)
-    nextWorker = sortedWorker[0]
+if __name__ == '__main__':
     
-    # print 'working: ',working
-    raw_input()
+    nextBuildings = getNextBuildings(now,working,obj,nextWorker)
+    # print 'nextBuildings: ',nextBuildings
+    maxCollision = getMaxCollision(nextBuildings,buildings,maxLoot)
+    # print 'getMaxCollision: ',maxCollision
+    time = 0
+    tempTI = 0
+    while not finish(now,working,obj):
+        timeInterval = getTimeInterval(nextWorker) + tempTI
+        tempTI = timeInterval
+        loot = addLoot(loot,earn,timeInterval)
+        time = addTime(time,timeInterval)
+        minInterval = getMinInterval(nextBuildings,buildings,earn)
+        maxInterval = getMaxInterval(nextBuildings,buildings,maxLoot,loot,earn)
+    #    print 'maxInterval: ',maxInterval,',minInterval: ',minInterval
+        available = findAvailable(nextBuildings,buildings,loot,minInterval,maxInterval,maxCollision,worker,working)
+    #    print 'available: ',available
+    #    print '>'*20
+    #    print 'timeInterval:',timeInterval
+    #    print 'time:',time
+    #    print 'tempTI:',tempTI
 
-print 'Done!'
+    #    print '<'*20
+
+        if available:
+            if len(available) == 1:
+                nextBuilding = available
+            else:
+                nextBuilding = findBest(available,working,buildings,earn,bound)
+        #    
+    #        print 'nextBuilding:',nextBuilding
+    #        worker = upWorker(nextBuilding,buildings,worker,timeInterval)
+            # print 'worker:',worker
+
+    #        now = upNow(working,now)
+    #        print 'now:',now
+
+    #        print '>> working', working
+    #        working = upWorking(working, nextBuilding,buildings,worker, timeInterval)
+    #        print '<< working:',working
+
+            require=getRequire(nextBuilding,buildings,loot)
+            
+            bn, blv = nextBuilding
+            print 'Start build:',bn
+            print 'level:',blv
+            print 'require:',buildings[bn][blv]['cost'],' ',buildings[bn]['type']
+            try:
+                print 'require per day:',[require[0]/timeInterval,require[1]/timeInterval,require[2]/timeInterval]
+            except ZeroDivisionError:
+                print 'wtf'
+            tempTI = 0
+            pausedTime=timeInterval
+        else:
+            pausedTime=getPausedTime(pausedTime)
+    #        worker=waitWorker(waitTime,worker)
+
+    #        working=waitWorking(waitTime,working)
+
+    #        now=examinigDone(working,now)
+    #        print 'else'
+            # no available building waiting resourse 
+            # TBD
+        print '===before==='
+        print 'worker:',worker
+        print 'working:',working
+        print 'now:',now
+        print '============'
+        updater(now, working, worker, nextBuilding, buildings, pausedTime)
+        print '===after==='
+        print 'worker:',worker
+        print 'working:',working
+        print 'now:',now
+        print '============'
+        sortedWorker = sorted(worker)
+        nextWorker = sortedWorker[0]
+        
+        # print 'working: ',working
+        raw_input()
+
+    print 'Done!'
